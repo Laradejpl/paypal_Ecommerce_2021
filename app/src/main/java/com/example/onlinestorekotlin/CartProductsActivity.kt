98897ce07dbb2fd4dc2c76400cb4e792835cdacc
laradejpl <51596185+Laradejpl@users.noreply.github.com>
2021-02-08 19:31:53 +0100

    mise en place du menubar pour temporary_order et mise en place des boutonspour les activity finalise,supression de NoActionBar sur le theme

--- app/src/main/java/com/example/onlinestorekotlin/CartProductsActivity.kt
+++ app/src/main/java/com/example/onlinestorekotlin/CartProductsActivity.kt
@@ -1,29 +1,90 @@
 package com.example.onlinestorekotlin
 
 
+import android.content.Intent
 import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+
 import android.view.WindowManager
 
 import android.widget.ArrayAdapter
+import android.widget.Toast
 import com.android.volley.Request
+import com.android.volley.Response
 
 import com.android.volley.toolbox.JsonArrayRequest
+import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import kotlinx.android.synthetic.main.activity_cart_products.*
 import kotlinx.android.synthetic.main.activity_home_screen.*
 
 class CartProductsActivity : AppCompatActivity() {
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+
+        menuInflater.inflate(R.menu.cart_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        if (item?.itemId == R.id.continueShoppingItem) {
+
+            var intent = Intent(this, HomeScreen::class.java)
+            startActivity(intent)
+        } else if (item?.itemId == R.id.declineOrderItem) {
+
+            val deleteUrl = "https://reggaerencontre.com/decline_order.php?email=${Person.email}"
+            val requestQ = Volley.newRequestQueue(this@CartProductsActivity)
+            val stringRequest = StringRequest(Request.Method.GET, deleteUrl, Response.Listener{
+                response ->
+
+
+                var intent = Intent(this, HomeScreen::class.java)
+                startActivity(intent)
+
+            }, Response.ErrorListener {
+                error ->
+
+
+
+            })
+
+            requestQ.add(stringRequest)
+        }else if (item?.itemId == R.id.verifyOrderItem) {
+
+
+            var verifyOrderUrl = "https://reggaerencontre.com/verify_order.php?email=${Person.email}"
+            var requestQ = Volley.newRequestQueue(this@CartProductsActivity)
+            var stringRequest = StringRequest(Request.Method.GET, verifyOrderUrl, {
+                response ->
+
+
+                val intent = Intent(this@CartProductsActivity, FinalizeShoppingActivity::class.java)
+                Toast.makeText(this, response, Toast.LENGTH_LONG).show()
+                intent.putExtra("LATEST_INVOICE_NUMBER", response)
+                startActivity(intent)
+
+
+            }, { error ->  })
+
+
+
+            requestQ.add(stringRequest)
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cart_products)
 
 
-        window.setFlags(
-            WindowManager.LayoutParams.FLAG_FULLSCREEN,
-            WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+
+
 
         val typeface: Typeface = Typeface.createFromAsset(assets,"Tabby - Display.ttf")
         title_cart_tv.typeface = typeface
@@ -66,6 +127,14 @@ class CartProductsActivity : AppCompatActivity() {
 
 
         requestQ.add(jsonAR)
+
+
+
+
+
+
+
+
     }
 
 
